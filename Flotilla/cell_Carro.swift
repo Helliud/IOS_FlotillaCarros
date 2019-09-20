26150deffe0fce73d0057144da479c70aca0cc7c
Alumno <ulsa@JesusCorralMAAAAC.local>
2019-09-20 14:23:02 -0700

    Porfin salio bien

---
+++ Flotilla/cell_Carro.swift
@@ -0,0 +1,24 @@
+//
+//  cell_Carro.swift
+//  Flotilla
+//
+//  Created by Alumno on 9/20/19.
+//  Copyright © 2019 ulsa. All rights reserved.
+//
+
+import UIKit
+
+class cell_Carro: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Flotilla/modelo_Carro.swift
@@ -0,0 +1,9 @@
+//
+//  modelo_Carro.swift
+//  Flotilla
+//
+//  Created by Alumno on 9/20/19.
+//  Copyright © 2019 ulsa. All rights reserved.
+//
+
+import Foundation
